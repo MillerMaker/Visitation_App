1d5cec2a61d35c0723fb257055e13022881fcf58
MillerMaker <cohen11miller@gmail.com>
2025-07-08 18:45:39 -0400

    Configuration Changes

--- visitation-backend/obj/Debug/net9.0/visitation-backend.AssemblyInfo.cs
+++ visitation-backend/obj/Debug/net9.0/visitation-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("visitation-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace36dfeed6a3e5a30f102b1b5b574a224514a22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caeff84438f30a8a262c8f4193803dd82519faa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("visitation-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("visitation-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
